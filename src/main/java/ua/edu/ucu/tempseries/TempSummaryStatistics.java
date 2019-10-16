94918240bce53b4ca595bd46ab2816a534431ed5
TytarenkoVictor <tytarenko.v@ucu.edu.ua>
2019-10-16 09:22:07 +0300

    TemperatureSeries initial commit

---
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -0,0 +1,5 @@
+package ua.edu.ucu.tempseries;
+
+public class TempSummaryStatistics {
+    
+}
---
+++ src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
@@ -0,0 +1,46 @@
+package ua.edu.ucu.tempseries;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Ignore;
+
+public class TemperatureSeriesAnalysisTest {
+
+    @Test
+    public void testAverageWithOneElementArray() {
+        // setup input data and expected result
+        double[] temperatureSeries = {-1.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = -1.0;
+
+        // call tested method
+        double actualResult = seriesAnalysis.average();
+
+        // compare expected result with actual result
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void testAverageWithEmptyArray() {
+        double[] temperatureSeries = {};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        // expect exception here
+        seriesAnalysis.average();
+    }
+
+    @Ignore
+    @Test
+    public void testAverage() {
+        double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = 1.0;
+
+        double actualResult = seriesAnalysis.average();
+        
+        assertEquals(expResult, actualResult, 0.00001);        
+    }
+    
+
+}
