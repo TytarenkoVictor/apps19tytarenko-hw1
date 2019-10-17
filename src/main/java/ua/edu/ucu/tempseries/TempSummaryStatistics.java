88cccadc4ea9ef404aa6c33fdfa919faeb123bb4
TytarenkoVictor <44294074+TytarenkoVictor@users.noreply.github.com>
2019-10-17 19:45:28 +0300

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,5 +1,26 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    
+    private double avgTemp;
+    private double devTemp;
+    private double minTemp;
+    private double maxTemp;
+    public TempSummaryStatistics(double avgTemp, double devTemp, double minTemp, double maxTemp) {
+        this.avgTemp = avgTemp;
+        this.devTemp = devTemp;
+        this.minTemp = minTemp;
+        this.maxTemp = maxTemp;
+    }
+    public double getAvgTemp() {
+        return avgTemp;
+    }
+    public double getDevTemp() {
+        return devTemp;
+    }
+    public double getMinTemp() {
+        return minTemp;
+    }
+    public double getMaxTemp() {
+        return maxTemp;
+    }
 }
